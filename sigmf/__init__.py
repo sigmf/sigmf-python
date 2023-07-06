da48c624e67f930e536dbb33716554c99b63c07a
Glen Mabey <gmabey@gmail.com>
2023-07-06 10:40:16 -0600

    incremented version to 1.1.2

--- sigmf/__init__.py
+++ sigmf/__init__.py
@@ -4,7 +4,7 @@
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 
 from .archive import SigMFArchive
 from .sigmffile import SigMFFile, SigMFCollection
