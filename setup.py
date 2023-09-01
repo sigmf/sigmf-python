83c0e0e70b3a74b98c17a163eed9e0226b065a13
Teque5 <teque5@gmail.com>
2023-08-31 21:53:18 -0700

    PyPi uses descriptions from setup, fix outdated entry.

--- setup.py
+++ setup.py
@@ -3,25 +3,22 @@ from setuptools import setup
 import os
 import re
 
-shortdesc = 'Signal Metadata Format Specification'
-longdesc = '''
-The Signal Metadata Format (SigMF) specifies a way to describe
-sets of recorded digital signal samples with metadata written in JSON.
-SigMF can be used to describe general information about a collection
-of samples, the characteristics of the system that generated the
-samples, and features of the signal itself.
-'''
+short_description = 'Python module for interacting with SigMF recordings.'
 
-with open(os.path.join('sigmf', '__init__.py')) as handle:
+with open('README.md', encoding='utf-8') as handle:
+    long_description = handle.read()
+
+with open(os.path.join('sigmf', '__init__.py'), encoding='utf-8') as handle:
     version = re.search(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]', handle.read()).group(1)
 
 setup(
     name='SigMF',
     version=version,
-    description=shortdesc,
-    long_description=longdesc,
+    description=short_description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/sigmf/sigmf-python',
-
+    license='GNU Lesser General Public License v3 or later (LGPLv3+)',
     classifiers=[
         'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
         'Operating System :: OS Independent',
@@ -47,6 +44,6 @@ setup(
         'gui': 'pysimplegui==4.0.0'
     },
     setup_requires=['pytest-runner'],
-    tests_require=['pytest>3'],
+    tests_require=['pytest>3', 'hypothesis'],
     zip_safe=False
 )
